1c33a3b4099b0e238323e5745600bc76ab28ef59
Dexter Lewis <dexter.lewis@gmail.com>
2019-04-18 18:19:27 -0400

    setup initial store

---
+++ friends/src/components/Login.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+export default class Login extends Component {
+    state={
+        username: '',
+        password: ''
+
+    }
+  render() {
+    return (
+      <div>
+        <h1>Hello from Login</h1>
+      </div>
+    )
+  }
+}
